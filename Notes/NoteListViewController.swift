1f1ece4d341da4bda9f3724eef6c07436203ddc9
Daniel Belokursky <daniel.belokursky@gmail.com>
2022-03-23 16:10:02 +0300

    init: start Notes App

---
+++ Notes/NoteListViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Notes
+//
+//  Created by Daniel Belokursky on 23.03.22.
+//
+
+import UIKit
+
+class NoteListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
